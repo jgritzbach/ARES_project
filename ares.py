3aaa7db1d4a6dcb5bf99ce45d5ad78d2870f5bd1
Jan Gritzbach <jgritzbach@localhost.localdomain>
2024-01-18 07:25:48 +0100

    Separating ico validation into three sub-methods
    Previously, the get_subject_by_ico() method took care in validating and manipulating the ico argument to be suited for ARES API.
    
    However, this was not aligned with SOC and SRP principles.
    
    ICO validation has been delegated into a separate method validate_ico(), which further delegates this task to _force_string() and _force_full_length() methods.
    
    The code is now more modular.

--- ares.py
+++ ares.py
@@ -2,15 +2,13 @@ import requests
 
 
 class AresApiClient:
-    """Retrieves and manipulates data from the Czech Republic's ARES register
+    """Class retrieves and manipulates data from the Czech Republic's ARES register
 
-    The lookup is based on unique ID number called "IČO" that each company in the Czech republic has
-    Since the word "IČO" contains czech diacritics, it is further replaced with "ICO" or "ico" in the code
-    The subject's data can be further manipulated by other methods
+    Request are based on a unique ID number called "IČO" that each company in the Czech republic has.
+    The word "IČO" contains czech diacritics. In the code, diacritics are ignored, naming variables "ICO" or "ico"
 
-    Methods:
-        get_subject_by_ico: This is "the core method" of the whole class. It returns subject data from ARES based on a given ICO
-        get_subject_formal_description: Returns a description of a subject in a way expected in formal written communication
+    This class relies on the ARES server REST Api settings to not change over time.
+    If these were to change in the future, the class might not work correctly
     """
 
     # "constants"
@@ -24,60 +22,83 @@ class AresApiClient:
         Looks up subject by its ICO IN ARES register REST Api endpoint
         Returns subject data as dictionary (with some nested dictionaries)
 
-        The ICO is always eight-digit. If less digits are passed, zeros are added before it up to full length
-        This is because lot of state institutions in the Czech Republic have ICO starting with several zeros
-        If ICO parameter is ever passed by a user input, it would be cumbersome to write them all
-
-        The result relies on the ARES server REST Api settings to not change over time
-        If request is not successfull, None is returned
-
         Args:
             ico (string or int): the unique ID number used to look up the subject in the ARES register
-
         Returns:
-            dictionary or None
-
-        Raises:
-            TypeError: if ico arg can't be converted to a string
-
+            dictionary of subject data (or None If request is not successful)
         """
-        try:
-            string_ico = str(ico)  # we force ico argument to be string even if it was passed as a number
-        except TypeError as e:
-            print(f'string conversion failed on {ico}: {e}')
-            return
+        ico = AresApiClient.validate_ico(ico)
+        if not ico:  # if ico did not passed validation process
+            return  # None is returned
 
-        # if less than 8 digits were passed, the length is insufficient
-        length_insufficient = AresManager.ICO_LENGTH - len(string_ico)
+        response = requests.get(AresApiClient.ARES_REQUEST_URL + ico)  # actually retrieving the data
 
-        if length_insufficient:  # zeros will be added before ico up to required length
-            string_ico = length_insufficient * "0" + string_ico
+        if response.status_code != 200:  # if request is not successful,
+            return  # None is returned
 
-        response = requests.get(AresManager.ARES_REQUEST_URL + string_ico)  # actually retrieving the data
-
-        if response.status_code != 200:
-            # if request is not successfull, None is returned
-            return
-
-        data = response.json()  # ARES gives data in JSON format. Using .json() results in a dictionary of values
+        data = response.json()  # ARES returns data in JSON format. Using .json() results in a dictionary of values
         return data
 
     @staticmethod
     def get_subject_formal_description(ico):
-        """Returns subject data in a way expected in a formal human written communication
+        """Returns subject data from ARES formatted as string in a way expected in a formal human written communication
 
-        Looks up the subject by its ICO IN ARES register
-        Returns some of its data that are considered to be descriptive enough
-        These are given by the Czech laws (based on circumstances) or simply by a common conventions
-        If subject is not successfully found, None is returned
+        Sometimes the Czech laws (or simply common conventions) require proper identification of the subject
+        Usually the whole name, IČO and full address is required to identify subject reliably
 
         Args:
             ico (string or int): the unique ID number used to look up the subject in the ARES register
 
         Returns:
-            string or None
-
+            string (or None If subject is not successfully found)
         """
-        data = AresManager.get_subject_by_ico(ico)  # retrieving all the data of the subject as a dictionary
-        if data:    # if succesfully retrieved, use only some of them
+        data = AresApiClient.get_subject_by_ico(ico)  # retrieving all the data of the subject as a dictionary
+        if data:  # if retrieved successfully, use only some of them
             return f'{data["obchodniJmeno"]}, IČO {data["ico"]}, sídlem {data["sidlo"]["textovaAdresa"]}'
+
+    @staticmethod
+    def validate_ico(ico):
+        """Mutates the ico to be suited for ARES API
+
+        Returns: string (or None if string conversion fails)
+        """
+        ico = AresApiClient._force_string(ico)  # Parses ico to a string, if not already
+        if not ico:  # if the string conversion failed
+            return  # return None
+
+        # ARES API requires 8-digit long parameter
+        ico = AresApiClient._force_full_length(ico)  # add zeros before ico up to full length
+
+        return ico
+
+    @staticmethod
+    def _force_string(ico):
+        """Parses ico to a string, if not already
+
+        Returns string (or None if the string conversion fails)
+        """
+        try:
+            ico = str(ico)  # we force ico argument to be string even if it was passed as a number
+        except TypeError as e:
+            print(f'string conversion failed on {ico}:\n{e}')
+            return
+
+        return ico
+
+    @staticmethod
+    def _force_full_length(ico):
+        """Adds zeros before ico up to full 8-digit length, if not already
+
+        Returns: string
+        """
+        #  The IČO is always eight-digit long and ARES API strictly requires it to be.
+        #  However, a lot of state institutions in the Czech Republic have IČO starting with several zeros
+        #  If ico parameter is acquired by a user input, it would be cumbersome for user to write them all manually
+        #  So we want to allow the user to skip those zeros, writing only the positive numbers that follow
+
+        length_insufficient = AresApiClient.ICO_LENGTH - len(ico)
+
+        if length_insufficient:
+            ico = length_insufficient * "0" + ico
+
+        return ico
